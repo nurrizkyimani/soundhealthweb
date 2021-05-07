533c61c6da895a0a68efb48654ffe3e6379d45da
nurrizkyimani <nurrizkyimanidev@gmail.com>
2021-05-07 15:34:39 +0700

    new commit with env var

--- frontend/config/fire-config.js
+++ frontend/config/fire-config.js
@@ -4,7 +4,13 @@ import firebase from 'firebase/app';
 try {
 	// Use your config values here.
 	firebase.initializeApp({
-	
+		apiKey: `${process.env.apiKey}`,
+		authDomain: `${process.env.authDomain}`,
+		projectId: `${process.env.projectId}`,
+		storageBucket: `${process.env.storageBucket}`,
+		messagingSenderId: `${process.env.messagingSenderId}`,
+		appId: `${process.env.appId}`,
+		measurementId: `${process.env.measurementId}`
 	});
 
 } catch (err) {
