4f27c3925064a1eb6d93ee064ca7e311ac87bf93
nurrizkyimani <nurrizkyimanidev@gmail.com>
2021-05-16 15:55:20 +0700

    add initial animation and permission modal

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -31,21 +31,66 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 import { useForm } from 'react-hook-form';
+import { motion, Frame, useAnimation } from 'framer-motion';
+
+function PermissionModal(props) {
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
+	useEffect(() => {
+		if (props.modalOpen == true) {
+				onOpen()
+		}
+
+		console.log(props.modalOpen);
+		
+	}, [props.modalOpen == true])
+	
+
+
+	function handleSubmitModal(event) {
+		event.preventDefault();
+		onClose()
+
+		props.setHiddenWelcome(true)
+	}
+	
+
+	return (
+		<Modal
+			size="lg"
+			isOpen={isOpen}
+			onClose={() => {
+				onClose();
+			}}
+		>
+			<ModalOverlay />
+			<ModalContent>
+				<form onSubmit={(event) => handleSubmitModal(event)}>
+					<ModalHeader>Informasi Subjek </ModalHeader>
+					<ModalCloseButton />
+					<ModalBody>setuju atau enggak</ModalBody>
+
+					<ModalFooter>
+						<div className="flex w-full justify-center">
+							<input type="submit" className={`px-10 py-4 text-left border rounded-full `} />
+						</div>
+					</ModalFooter>
+				</form>
+			</ModalContent>
+		</Modal>
+	);
+}
 
 export default function Home() {
 	const [ isAudioOn, setIsAudioOn ] = useState(false);
-
 	const [ isUpload, setIsUpload ] = useState(false);
-
 	const { isOpen, onOpen, onClose } = useDisclosure();
 
 	const { register, handleSubmit } = useForm();
-
 	const [ audioUrl, setAudioUrl ] = useState('');
-
 	const [ userData, setUserData ] = useState(null);
 
-	// let recorder;
+	const [hiddenWelcome, setHiddenWelcome] = useState(false);
 
 	useEffect(() => {
 		function mediaRec() {
@@ -60,27 +105,6 @@ export default function Home() {
 		}
 		mediaRec();
 	}, []);
-
-	const onSubmit = async (data) => {
-		const condition = data.condition.split(',');
-		console.log({ ...data, condition });
-
-		const newdata = {
-			age: data.age,
-			gender: data.gender,
-			symptoms: data.symptoms,
-			condition: data.condition.split(','),
-			isCovid: data.suspek,
-			url: audioUrl
-		};
-
-		console.log(newdata);
-
-		fire.firestore().collection('coughdataset').add(newdata).then(() => {
-			setUserData(newdata);
-			onClose();
-		});
-	};
 
 	const options = [ { label: 'Ya', id: '1' }, { label: 'Tidak', id: '2' } ];
 	const genderOptions = [
@@ -99,35 +123,26 @@ export default function Home() {
 
 	useEffect(
 		() => {
+			//send the blob function,
 			const setblob = async (url) => {
-				const soundRef = fire.storage().ref();
 				let metadata = {
 					contentType: 'audio/wav'
 				};
+				//fetch the blob from the browser local storage;
 				const blob = await fetch(mediaBlobUrl).then((r) => r.blob());
-
+				//creating ref from storage ref
 				const storageRef = fire.storage().ref();
-
+				//get mediaurl uuid parse from the uri;
 				const uuid = mediaBlobUrl.substring(mediaBlobUrl.lastIndexOf('/') + 1);
-
+				//storage cloud reference
 				const soundref = storageRef.child(`sound/soundhealth-${uuid}.wav`);
 
+				//get the url then put it into the firebase;
 				soundref.put(blob, metadata).then((res) => {
-					console.log(res);
-
 					soundref.getDownloadURL().then((burl) => {
 						setAudioUrl(burl);
-							setUserData({...userData, url : burl})
+						setUserData({ ...userData, url: burl });
 					});
-				
-			
-				
-				if (userData != null) {
-					fire.firestore().collection('coughdataset').add(userData).then(() => {
-						console.log('another sound withuot submit the voice');
-						console.log(userData);
-				});
-			}
 				});
 			};
 
@@ -135,6 +150,7 @@ export default function Home() {
 				setblob(mediaBlobUrl);
 			}
 		},
+		//when the media url is exist;
 		[ mediaBlobUrl ]
 	);
 
@@ -152,17 +168,53 @@ export default function Home() {
 			//stop recording
 			stopRecording();
 
-			//modal is open; for user isi infonya
-
 			console.log(userData);
 
 			if (userData == null) {
 				onOpen();
+			} else if (userData != null) {
+				console.log('send user data without open the dialog; handleaudioonoff');
+				fire.firestore().collection('coughdataset').add(userData).then((ress) => {
+					console.log('another sound withuot submit the voice');
+					console.log(ress);
+				});
 			}
-
-			//send the recording to the blob in firebase
 		}
 	};
+
+	const onSubmit = async (data) => {
+		const condition = data.condition.split(',');
+		console.log({ ...data, condition });
+
+		const newdata = {
+			age: data.age,
+			gender: data.gender,
+			symptoms: data.symptoms,
+			condition: data.condition.split(','),
+			isCovid: data.suspek,
+			url: audioUrl
+		};
+
+		console.log(newdata);
+
+		fire
+			.firestore()
+			.collection('coughdataset')
+			.add(newdata)
+			.then(() => {
+				setUserData(newdata);
+				onClose();
+			})
+			.then(() => {
+				setIsUpload(true);
+			});
+	};
+
+
+	const [onModalOpen, setonModalOpen] = useState(false)
+
+	// let anim = useAnimation()
+
 
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -172,9 +224,20 @@ export default function Home() {
 			</Head>
 
 			<main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-				<h1 hidden={true} className="text-6xl font-bold max-w-xl text-justify">
-					Welcome to SoundHealth!
-				</h1>
+				<motion.div
+					animate={{
+						scale: 1.5
+					}}
+					transition={{ duration: 1 }}
+					onAnimationComplete={() => setonModalOpen(!onModalOpen)}
+				>
+					<h1 hidden={hiddenWelcome} className="text-4xl font-bold max-w-xl text-justify">
+						Welcome to SoundHealth!
+					</h1>
+				</motion.div>
+
+
+				<PermissionModal modalOpen={onModalOpen} setHiddenWelcome={setHiddenWelcome}/>
 
 				<h1 hidden={true} className="text-6xl font-bold max-w-xl text-justify">
 					SoundHealth is a deep learning project based that detects cough sound!
@@ -190,8 +253,7 @@ export default function Home() {
 
 				<div>
 					<p>{status}</p>
-					<button onClick={startRecording}>Start Recording</button>
-					<button onClick={stopRecording}>Stop Recording</button>
+
 					<audio src={mediaBlobUrl} controls autoPlay />
 				</div>
 
@@ -206,15 +268,6 @@ export default function Home() {
 					</div>
 				</div>
 
-				<div
-					className={`px-10 py-4 text-left border rounded-full mt-10`}
-					onClick={() => {
-						onOpen();
-					}}
-				>
-					<h1 className="text-2xl font-bold max-w-xl text-justify"> {isUpload ? 'Kirim' : 'Rekam'}</h1>
-				</div>
-
 				<Modal
 					size="lg"
 					isOpen={isOpen}
@@ -223,9 +276,10 @@ export default function Home() {
 					}}
 				>
 					<ModalOverlay />
-
+ss
 					<ModalContent>
-						<form onSubmit={handleSubmit(onSubmit)}>
+						<form
+							onSubmit={handleSubmit(onSubmit)}>
 							<ModalHeader>Informasi Subjek </ModalHeader>
 							<ModalCloseButton />
 							<ModalBody>
